bc6235676957e09a5d3ff9d961feebf854553ed1
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-07-25 00:34:17 +0930

    Handle multiple authors, and thumbnails

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,11 +1,24 @@
 import styles from "./BookCard.module.scss";
 
-const BookCard = ({ title, authors, description }) => {
+const BookCard = ({ title, authors, description, thumbnail }) => {
     return (
         <div className={styles.BookCard}>
+            <img
+                src={thumbnail}
+                alt={
+                    title && thumbnail !== ""
+                        ? `${title} bookcover`
+                        : "No image provided"
+                }
+            />
             <h2 id="title">{title}</h2>
             <p id="author">
-                <strong>Author:</strong> {authors}
+                <strong>Author:</strong>{" "}
+                {authors !== undefined
+                    ? authors.length > 1
+                        ? authors.join(", ")
+                        : authors
+                    : ""}
             </p>
             <p id="description">
                 <strong>Description:</strong> {description}
