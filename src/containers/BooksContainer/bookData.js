1393a0f91f54d1425de88505b46b2088122ff93d
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-07-25 00:32:49 +0930

    Match dummy data to Google api

--- src/containers/BooksContainer/bookData.js
+++ src/containers/BooksContainer/bookData.js
@@ -1,27 +1,37 @@
 export const bookData = [
     {
         title: "Lord Of The Rings",
-        authors: "JRR Tolkien",
+        authors: ["JRR Tolkien"],
         description: "A hobbit and his friends have to dispose of a ring.",
+        thumbnail:
+            "http://books.google.com/books/content?id=ptHZAAAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
     },
     {
         title: "The Hobbit",
-        authors: "JRR Tolkien",
+        authors: ["JRR Tolkien"],
         description: "A hobbit finds a ring.",
+        thumbnail:
+            "http://books.google.com/books/content?id=6tLdzgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
     },
     {
         title: "Pride and Prejudice",
-        authors: "Jane Austen",
+        authors: ["Jane Austen"],
         description: "A woman and a man have an awkward meet cute.",
+        thumbnail:
+            "http://books.google.com/books/content?id=s1gVAAAAYAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
     },
     {
         title: "Watership Down",
-        authors: "Richard Adams",
+        authors: ["Richard Adams"],
         description: "A group of rabbits must find a new home.",
+        thumbnail:
+            "http://books.google.com/books/content?id=ittzoegmRpAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
     },
     {
         title: "1984",
-        authors: "George Orwell",
+        authors: ["George Orwell"],
         description: "Winston is having a very bad time.",
+        thumbnail:
+            "http://books.google.com/books/content?id=aPlAtgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
     },
 ];
