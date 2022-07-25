de5817743f15842759e4a39c282d7fb495ce1e59
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-07-25 21:31:12 +0930

    Update to use BEM

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -3,8 +3,15 @@ import styles from "./SearchForm.module.scss";
 const SearchForm = ({ searchBooks }) => {
     return (
         <form id="searchForm" className={styles.SearchForm}>
-            <input id="searchStr" type="text" placeholder="Enter a book..." />
-            <button onClick={searchBooks}>Search</button>
+            <input
+                id="searchStr"
+                className={styles.SearchForm__Input}
+                type="text"
+                placeholder="Enter a book..."
+            />
+            <button className={styles.SearchForm__Button} onClick={searchBooks}>
+                Search
+            </button>
         </form>
     );
 };
